b9eb47b687c08fb2a29f0a6e24e1363e5b559b4d
Simonov Denis <sim-mail@list.ru>
2023-05-26 16:02:40 +0300

    include docs into builds

--- projects/http_client_udr/release_pack-x64.bat
+++ projects/http_client_udr/release_pack-x64.bat
@@ -4,5 +4,5 @@ set BUILD_DIR=%1build\http_client_udr\windows-x64\Release
 
 del "%OUTPUT_DIR%\HttpClientUdr_Win_x64.zip"
 "%ARCH_DIR%\7z.exe" a -tzip "%OUTPUT_DIR%\HttpClientUdr_Win_x64.zip" "%BUILD_DIR%\http_client_udr.dll" "%BUILD_DIR%\libcurl.dll" "%BUILD_DIR%\zlib1.dll" ^
- "%1README.md" "%1sql"
+ "%1README.md" "%1README_RU.md" "%1sql"
 
